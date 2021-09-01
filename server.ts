c7d18667976616c512f2ae750ba6fcebdae68a35
Tal <tal.yaron@gmail.com>
2021-09-01 20:48:56 +0300

    path 4

--- server.ts
+++ server.ts
@@ -6,11 +6,11 @@ const path = require('path');
 
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, "client","build")));
-app.use(express.static('client/build'));
+// app.use(express.static('client/build'));
+app.use('/static', express.static(path.join(__dirname, 'client','build')));
 
 
-
-console.log(path.join(__dirname, "client","build"));
+console.log(path.join(__dirname, "client","build",'aaa'));
 
 
 app.listen(port, () => { console.log('Server listen on port', port) });
