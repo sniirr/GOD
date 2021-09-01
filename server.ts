c8a818ee331a85cc5001b934ddffa5e72d9b28ac
Tal <tal.yaron@gmail.com>
2021-09-01 20:41:38 +0300

    path 2

--- server.ts
+++ server.ts
@@ -3,13 +3,13 @@ const app = express();
 const port:number|string = process.env.PORT || 4000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, "client","build")));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client","build")));
 
-import {secret} from './secret';
 
 
-console.log(secret);
+console.log(path.join(__dirname, "client","build"));
 
 
 app.listen(port, () => { console.log('Server listen on port', port) });
