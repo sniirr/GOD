419939ef7443878259f131786cc5cf76ca04b592
Tal <tal.yaron@gmail.com>
2021-10-01 16:20:59 +0300

    indicator works

--- client/src/views/components/TopIndicator/TopIndecator.tsx
+++ client/src/views/components/TopIndicator/TopIndecator.tsx
@@ -13,13 +13,17 @@ export interface TopIndecatorProps {
 
 const TopIndecator: FC<TopIndecatorProps> = (props:TopIndecatorProps) => {
     const { pages, path } = props;
-
+    let { url } = useRouteMatch();
+    let urlSplit = url.split('/');
+    let pageFromUrl = urlSplit[urlSplit.length-1];
+ console.log(pageFromUrl);
  
     return (
         <div className="indicator">
             <hr></hr>
             {pages.map((page, i)=>{
-                return(<Link key={i} to={`${path}/${page}`}><div>{i+1}</div></Link>);
+                console.log(page===pageFromUrl)
+                return(<Link key={i} to={`${path}/${page}`}><div className={page===pageFromUrl ?'indicator--current':undefined}>{i+1}</div></Link>);
             })}
             
         </div>
