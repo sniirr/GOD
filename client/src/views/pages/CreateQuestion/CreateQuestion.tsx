b45e1739cd236ce3c2be6d046aad3b47d42c6ae8
Tal <tal.yaron@gmail.com>
2021-10-01 09:16:36 +0300

    init solution

--- client/src/views/pages/CreateQuestion/CreateQuestion.tsx
+++ client/src/views/pages/CreateQuestion/CreateQuestion.tsx
@@ -1,4 +1,12 @@
 import React, { FC, ReactElement, useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams,
+    useRouteMatch
+} from "react-router-dom";
 
 import CreateQuestion1 from './CreateQuestion1';
 import CreateQuestion2 from './CreateQuestion2';
@@ -7,29 +15,67 @@ import CreateQuestion4 from './CreateQuestion4';
 
 export interface createQuestionProps {
     position: number;
-    setPosition:React.Dispatch<React.SetStateAction<number>>;
+    setPosition: React.Dispatch<React.SetStateAction<number>>;
 }
 
 
 const CreateQuestion: FC = () => {
     const [position, setPosition] = useState(1);
+    let { path, url } = useRouteMatch();
 
     return (
         <div>
-            {setPage(position, setPosition)}
+            <ul>
+                <li>
+                    <Link to={`${url}/1`}>1</Link>
+                </li>
+                <li>
+                    <Link to={`${url}/2`}>2</Link>
+                </li>
+                <li>
+                    <Link to={`${url}/3`}>3</Link>
+                </li>
+                <li>
+                    <Link to={`${url}/4`}>4</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path={path}>
+                    <h3>Please select a topic.</h3>
+                </Route>
+                <Route path={`${path}/:topicId`}>
+                    <Topic />
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+const Topic: FC = () => {
+    // The <Route> that rendered this component has a
+    // path of `/topics/:topicId`. The `:topicId` portion
+    // of the URL indicates a placeholder that we can
+    // get from `useParams()`.
+    //
+    // eslint-disable-next-line
+    let { topicId } = useParams<{ topicId: string }>();
+
+    return (
+        <div>
+            <h3>{topicId}</h3>
         </div>
     );
 }
 
 // eslint-disable-next-line
-function setPage (position:number, setPosition:React.Dispatch<React.SetStateAction<number>>):ReactElement {
+function setPage(position: number, setPosition: React.Dispatch<React.SetStateAction<number>>): ReactElement {
     console.log(position);
 
-    switch(position){
-        case 1: return <CreateQuestion1 position={position} setPosition={setPosition}/>;
-        case 2: return <CreateQuestion2 position={position} setPosition={setPosition}/>;
-        case 3: return <CreateQuestion3 position={position} setPosition={setPosition}/>;
-        case 4: return <CreateQuestion4 position={position} setPosition={setPosition}/>;
+    switch (position) {
+        case 1: return <CreateQuestion1 position={position} setPosition={setPosition} />;
+        case 2: return <CreateQuestion2 position={position} setPosition={setPosition} />;
+        case 3: return <CreateQuestion3 position={position} setPosition={setPosition} />;
+        case 4: return <CreateQuestion4 position={position} setPosition={setPosition} />;
         default: return <CreateQuestion1 position={position} setPosition={setPosition} />;
     }
 }
