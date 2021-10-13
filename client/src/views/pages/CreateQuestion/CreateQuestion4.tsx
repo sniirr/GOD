879c3836c17db67beede1a1f0f75e38b31faf3f2
Tal <tal.yaron@gmail.com>
2021-10-13 13:40:21 +0300

    starting share page

--- client/src/views/pages/CreateQuestion/CreateQuestion4.tsx
+++ client/src/views/pages/CreateQuestion/CreateQuestion4.tsx
@@ -1,16 +1,32 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+//material UI components
 import Button from '@mui/material/Button';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 import { createQuestionProps } from './CreateQuestion';
 
 const CreateQuestion4: FC<createQuestionProps> = (props: createQuestionProps) => {
-   
+    const { path } = props;
 
     return (
         <div>
-            <h1>CreateQuestion 4</h1>
-            <Button onClick={()=>{}}>Next </Button>
+            <div className="wrapper">
+                <h1>Share it</h1>
+                <p>This is the moment your idea is sent out to the world. Finish the process by Inviting the people relevant to solving this issue., Together you will reach a better solution.
+                </p>
+
+            </div>
+            <div className="bottomNavButtons">
+                <Link to={`${path}/2`}>
+                    <Button variant="outlined" startIcon={<ArrowBackIosIcon />}>Back</Button>
+                </Link>
+                
+            </div>
         </div>
+        
     );
 }
 
