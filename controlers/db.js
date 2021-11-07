1f65c695e8cf511974e8f6cf6bea99343c4b72e8
Tal <tal.yaron@gmail.com>
2021-11-07 18:58:09 +0200

    cont

--- controlers/db.js
+++ controlers/db.js
@@ -7,7 +7,7 @@ console.log(a);
 var mongoose = require('mongoose');
 //Set up default mongoose connection
 //local:
-// const mongoDB = 'mongodb://127.0.0.1/my_database'; 
+// const mongoDB = 'mongodb://127.0.0.1/global_online'; 
 //atlas:
 var mongoDB = 'mongodb+srv://tal1:9jNgEw4y7l9wjkkI@tal-test1.m39if.mongodb.net/global_online?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
--- controlers/db.ts
+++ controlers/db.ts
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 //Set up default mongoose connection
 //local:
-// const mongoDB = 'mongodb://127.0.0.1/my_database'; 
+// const mongoDB = 'mongodb://127.0.0.1/global_online'; 
 //atlas:
 const mongoDB = 'mongodb+srv://tal1:9jNgEw4y7l9wjkkI@tal-test1.m39if.mongodb.net/global_online?retryWrites=true&w=majority';
 
